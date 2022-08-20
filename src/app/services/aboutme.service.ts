b03efa9d0445c83413317fb782d397ac9916a8cf
Sebastián Ibañez <77544506+Z3belek@users.noreply.github.com>
2022-08-20 17:11:36 -0300

    About
    Added service

--- src/app/services/aboutme.service.ts
+++ src/app/services/aboutme.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AboutmeService {
+  url = "https://portfolio-ap-v2.herokuapp.com/api/aboutme"
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getAboutme(): Observable<any> {
+    return this.http.get<Aboutme[]>(this.url);
+  }
+
+  editAboutme(id: number, aboutme: Aboutme): Observable<any> {
+    return this.http.put(this.url + '/' + id, aboutme);
+  }
+}
+
+export interface Aboutme {
+  dtext: string,
+  percentage1: number,
+  percentage2: number,
+  percentage3: number,
+  tpercentage1: string,
+  tpercentage2: string,
+  tpercentage3: string,
 }
