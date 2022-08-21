396fa2d6b07c79644a776a2fb9cd8a2a45b75c27
Sebastián Ibañez <77544506+Z3belek@users.noreply.github.com>
2022-08-21 03:01:25 -0300

    education ui
    Added new ui for education, and services

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { ConfirmService } from 'src/app/services/confirm.service';
+import { EducationService } from 'src/app/services/education.service';
+import { EduDialogComponent } from './edu-dialog/edu-dialog.component';
 
 @Component({
   selector: 'app-education',
@@ -6,10 +12,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.scss']
 })
 export class EducationComponent implements OnInit {
-
-  constructor() { }
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  educations: any = [];
+  constructor(
+    private educationService: EducationService,
+    public afAuth: AngularFireAuth,
+    public dialog: MatDialog,
+    private confirm: ConfirmService,
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+    this.listarEducation();
   }
 
+  openAddEdu() {
+    this.dialog.open(EduDialogComponent, {
+      panelClass: "dialog-responsive",
+    });
+  }
+
+  openEditEdu(education: any) {
+    this.dialog.open(EduDialogComponent, {
+      panelClass: "dialog-responsive",
+      data: education
+    })
+  }
+
+  openDeleteEdu(id: number) {
+    this.confirm
+      .confirmDialog({
+        title: 'Delete education',
+        message: 'Are you sure to delete this education?',
+        confirmCaption: 'Yes',
+        cancelCaption: 'No',
+      })
+      .subscribe((yes) => {
+        if (yes) this.educationService.deleteEducation(id).subscribe(
+          res => {
+            this.ngOnInit();
+            let config = new MatSnackBarConfig
+            config.panelClass = ['green-snackbar'];
+            config.verticalPosition = this.verticalPosition;
+            config.horizontalPosition = this.horizontalPosition;
+            config.duration = 1500;
+            this._snackBar.open("Removed successfully", "", config);
+          },
+          err => console.log(err)
+        );
+      });
+  }
+
+  listarEducation() {
+    this.educationService.getEducation().subscribe(
+      res => {
+        this.educations = res;
+        console.log(res);
+      },
+      err => console.log(err)
+    );
+  }
 }
--- src/app/services/education.service.ts
+++ src/app/services/education.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EducationService {
+  url = "https://portfolio-ap-v2.herokuapp.com/api/education"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getEducation(): Observable<any> {
+    return this.http.get<Education[]>(this.url);
+  }
+
+  getOneEducation(id: number): Observable<any> {
+    return this.http.get(this.url + '/' + id);
+  }
+
+  saveEducation(education: Education): Observable<Education> {
+    return this.http.post<Education>(this.url, education);
+  }
+
+  editEducation(id: number, education: Education): Observable<any> {
+    return this.http.put(this.url + '/' + id, education);
+  }
+
+  deleteEducation(id: number): Observable<any> {
+    return this.http.delete(this.url + '/' + id);
+  }
+}
+
+export interface Education {
+  id?: number,
+  type: string,
+  title: string,
+  institution: string,
+  imgurl: string,
+  haveurl: boolean,
+  urlcertificate: string,
+  dtext: string
 }
