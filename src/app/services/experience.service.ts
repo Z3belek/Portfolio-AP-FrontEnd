dbcafca23990d36097c09131d607ebf7f78315b1
Sebastián Ibañez <77544506+Z3belek@users.noreply.github.com>
2022-08-20 21:26:39 -0300

    Service
    added Experience service

--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExperienceService {
+  url = "https://portfolio-ap-v2.herokuapp.com/api/experience"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getExperience(): Observable<any> {
+    return this.http.get<Experience[]>(this.url);
+  }
+
+  getOneExperience(id: number): Observable<any> {
+    return this.http.get(this.url + '/' + id);
+  }
+
+  saveExperience(experience: Experience): Observable<Experience> {
+    return this.http.post<Experience>(this.url, experience);
+  }
+
+  editExperience(id: number, experience: Experience): Observable<any> {
+    return this.http.put(this.url + '/' + id, experience);
+  }
+
+  deleteExperience(id: number): Observable<any> {
+    return this.http.delete(this.url + '/' + id);
+  }
+}
+
+export interface Experience {
+  id?: number,
+  role: string,
+  place: string,
+  year1: string,
+  year2: string,
+  technologies: string[],
+  responsibilities: string[]
 }
